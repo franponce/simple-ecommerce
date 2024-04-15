e1bce0f433d5898b105d3d7eaf8fcce6ea6deccd
El diego <franponce98@gmail.com>
2024-04-15 15:37:31 -0300

    Checkout structure is created

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -24,35 +24,69 @@ export default function () {
                             <Link href="/" className="underline mb-5">
                                 Continuar comprando
                             </Link>
-                        
-                        {/* Items del carrito */}
-                        {
-                            productsInCart.map(product => (
-                                <div key={product.slug} className="flex">
-                                    <Image
-                                        src={`/products/${product.images[0]}`}
-                                        width={100}
-                                        height={100}
-                                        alt={product}
-                                        className="mr-5 rounded"
-                                    />
 
-                                    <div>
-                                        <p>{product.title}</p>
-                                        <p>${product.price}</p>
-                                        <QuantitySelector quantity={3} />
+                            {/* Items del carrito */}
+                            {
+                                productsInCart.map(product => (
+                                    <div key={product.slug} className="flex mb-5">
+                                        <Image
+                                            src={`/products/${product.images[0]}`}
+                                            width={100}
+                                            height={100}
+                                            style={{
+                                                width: '100px',
+                                                height: '100px'
+                                            }}
+                                            alt={product}
+                                            className="mr-5 rounded"
+                                        />
 
-                                        <button className="underline mt-3">
-                                            Remover
-                                        </button>
+                                        <div>
+                                            <p>{product.title}</p>
+                                            <p>${product.price}</p>
+                                            <QuantitySelector quantity={3} />
 
+                                            <button className="underline mt-3">
+                                                Remover
+                                            </button>
+
+                                        </div>
                                     </div>
-                                </div>
-                            ))
-                        }
+                                ))
+                            }
                         </div>
 
-                        {/* Checkout */}
+                        {/* Checkout - Resumen de compra */}
+                        <div className="bg-white rounded-xl shadow-xl p-7">
+                            <h2 className="text-2xl mb-2">Resumen de compra</h2>
+
+                            <div className="grid grid-cols-2">
+
+                                <span>No, Productos</span>
+                                <span className="text-right">3 articulos</span>
+
+                                <span>Subtotal</span>
+                                <span className="text-right">$ 100</span>
+
+                                <span>Envio</span>
+                                <span className="text-right">$ 100</span>
+
+                                <span className="mt-5 text-2xl">Total:</span>
+                                <span className="mt-5 text-2xl text-right">$ 200</span>
+
+                            </div>
+
+                            <div className="mt-5 mb-2 w-full">
+                                <Link
+                                    className="flex btn-primary justify-center"
+                                    href="/checkout/address">
+                                    Checkout
+                                </Link>
+                            </div>
+
+
+                        </div>
+
 
 
                     </div>
